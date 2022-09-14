d868ffff911b4ed1463d136d8943e8dafe238d68
pavlopetr <rezar66612@gmail.com>
2022-09-14 17:44:41 +0300

    hw

---
+++ README.md
@@ -0,0 +1,1 @@
+https://monosnap.com/file/D1XaBxSMN2pJyXF7YsyxJPCIOv1YKe
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('hello word')
+const { Command } = require('commander');
+const { listContacts, getContactById, addContact, removeContact } = require('./contacts');
+
+const program = new Command();
+
+program
+   .option('-a, --action <type>', 'choose action')
+   .option('-i, --id <type>', 'user id')
+   .option('-n, --name <type>', 'user name')
+   .option('-e, --email <type>', 'user email')
+   .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+   switch (action) {
+      case 'list':
+         const contactsList = await listContacts();
+         console.table(contactsList);
+         break;
+
+      case 'get':
+         const getContact = await getContactById(id);
+         console.log(getContact);
+         break;
+
+      case 'add':
+         const newContactList = await addContact(name, email, phone);
+         console.log(newContactList);
+         break;
+
+      case 'remove':
+         const contactToRemove = await removeContact(id);
+         console.log(contactToRemove);
+         break;
+
+      default:
+         console.warn('\x1B[31m Unknown action type!');
+   }
+}
+
+invokeAction(argv);
